1e436720c4d628fd27663b01ec9e4600def0b2e9
Chelsea Spangler <chelseanspangler@gmail.com>
2020-09-09 16:31:22 -0700

    .env configured; book insert test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,10 @@ const app = require('../lib/app');
 
 describe('sql-model-relationships routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+  it('makes the test pass', () => {
+    const value = 1;
+    expect(value).toEqual(1);
   });
 });
